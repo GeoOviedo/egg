7d4128a3442fd23dfa4fe9948f68041d4cefd8f1
Georgina Oviedo <geoco0710@gmail.com>
2021-10-15 19:27:31 -0300

    primer commit

---
+++ src/pruebagit/PruebaGit.java
@@ -0,0 +1,13 @@
+
+package pruebagit;
+
+public class PruebaGit {
+
+    public static void main(String[] args) {
+        
+        String cadena = "hola como estas";
+        
+        
+    }
+    
+}
